c220e46cc65381f94a173749e60a341d39063c97
shwe yee <yonngelay@gmail.com>
2019-07-03 16:53:59 +0800

    Fetching API data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React,  from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
   const APP_ID = '565ccdb5';
   const APP_KEY = '7673504cb19be659b79b42ea454ae095';
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
-    
-  });
+    getRecipes();
+  }, []);
 
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = await response.json();
+    console.log(data.hits);
+    setRecipes(data.hits);
+  };
   return (
     <div className="App">
       <form className="search-form">
@@ -17,6 +26,7 @@ const App = () => {
         <button className="search-button" type="submit">
           Search
         </button>
+        {/* <h1 onClick={() => setCounter(counter + 1)}>{counter}</h1> */}
       </form>
     </div>
   );
