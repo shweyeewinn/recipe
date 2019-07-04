36938715c69c4119d24d328249f384b7e8cb3290
shwe yee <yonngelay@gmail.com>
2019-07-04 11:07:10 +0800

    Adding list style css

--- src/Recipe.js
+++ src/Recipe.js
@@ -7,7 +7,7 @@ const Recipe = ({ title, calories, image, ingredients }) => {
       <h1>{title}</h1>
       <ol>
         {ingredients.map(ingredient => (
-          <li>{ingredient.text}</li>
+          <li className={style.list}>{ingredient.text}</li>
         ))}
       </ol>
       <p>{calories}</p>
